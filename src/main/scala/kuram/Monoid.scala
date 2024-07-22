015dcdd41bf2907a00c1c2a54ea391c077728e40
csgn <dev.csgn@gmail.com>
2024-07-22 13:05:46 +0300

    [feat] add new monoid instances

--- src/main/scala/kuram/Monoid.scala
+++ src/main/scala/kuram/Monoid.scala
@@ -40,3 +40,25 @@ object MonoidInstances:
     def empty: List[A] = Nil
     def combine(a: List[A], b: List[A]): List[A] = a ++ b
 
+  given unionSetMonoid[A]: Monoid[Set[A]] with
+    def empty: Set[A] = Set.empty[A]
+    def combine(a: Set[A], b: Set[A]): Set[A] = a | b
+  
+  given intersectSetMonoid[A]: Monoid[Set[A]] with
+    def empty: Set[A] = Set.empty[A]
+    def combine(a: Set[A], b: Set[A]): Set[A] = a & b
+
+  given [A: Monoid]: Monoid[Option[A]] with
+    def empty: Option[A] = None
+    def combine(a: Option[A], b: Option[A]): Option[A] = (a, b) match
+      case (Some(a1), Some(b1)) => Some(Monoid[A].combine(a1, b1))
+      case (a1, None) => a1
+      case (None, b1) => b1
+
+  given [K, V: Monoid]: Monoid[Map[K, V]] with
+    def empty: Map[K, V] = Map.empty[K, V]
+    def combine(a: Map[K, V], b: Map[K, V]): Map[K, V] =
+      (a.keySet ++ b.keySet).foldRight(Map.empty[K, V]): (key, acc) =>
+        acc.updated(key, Monoid[V].combine(
+                          a.getOrElse(key, Monoid[V].empty),
+                          b.getOrElse(key, Monoid[V].empty)))
