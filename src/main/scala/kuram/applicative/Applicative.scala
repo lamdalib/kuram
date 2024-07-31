32e4cde20b540fc155c10aa3180cb49dbb7946ec
csgn <dev.csgn@gmail.com>
2024-07-31 15:17:52 +0300

    feat: add new combinator to Applicative

--- src/main/scala/kuram/applicative/Applicative.scala
+++ src/main/scala/kuram/applicative/Applicative.scala
@@ -30,20 +30,41 @@ import apply.Apply
 trait Applicative[F[_]] extends Apply[F] {
 
   /** Convert any value into Applicative.
-   *
-   * Example:
-   * {{{
-   * scala> import kuram.applicative.Applicative
-   * scala> import kuram.applicative.instances.list.given
-   *
-   * scala> Applicative[List].pure(1)
-   * res0: List[Int] = List(1)
-   * }}}
+    *
+    * Example:
+    * {{{
+    * scala> import kuram.applicative.Applicative
+    * scala> import kuram.applicative.instances.list.given
+    *
+    * scala> Applicative[List].pure(1)
+    * res0: List[Int] = List(1)
+    * }}}
     */
   def pure[A](a: => A): F[A]
 
   extension [A](fa: F[A]) {
-    def map[B](f: A => B): F[B] =
+    def map6[B, C, D, E, G, Z](fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G])(f: (A, B, C, D, E, G) => Z): F[Z] =
+      product(fa, fb).map3(product(fc, fd), product(fe, fg)) { case ((a, b), (c, d), (e, g)) =>
+        f(a, b, c, d, e, g)
+      }
+
+    def map5[B, C, D, E, Z](fb: F[B], fc: F[C], fd: F[D], fe: F[E])(f: (A, B, C, D, E) => Z): F[Z] =
+      product(fa, fb).map2(product2(fc, fd, fe)) { case ((a, b), (c, d, e)) =>
+        f(a, b, c, d, e)
+      }
+
+    def map4[B, C, D, Z](fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => Z): F[Z] =
+      product(fa, fb).map2(product(fc, fd)) { case ((a, b), (c, d)) =>
+        f(a, b, c, d)
+      }
+
+    def map3[B, C, Z](fb: F[B], fc: F[C])(f: (A, B, C) => Z): F[Z] =
+      ap(fa.map2(fb)((a, b) => f(a, b, _)))(fc)
+
+    def map2[B, Z](fb: F[B])(f: (A, B) => Z): F[Z] =
+      ap(fa.map(f.curried))(fb)
+
+    override def map[B](f: A => B): F[B] =
       ap(pure(f))(fa)
   }
 }
