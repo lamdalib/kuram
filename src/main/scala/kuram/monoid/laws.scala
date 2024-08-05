89b9d9e97e4afe77521cb0882df9e064e6cc9b88
csgn <dev.csgn@gmail.com>
2024-08-05 19:36:30 +0300

    chore: Unimportant change

--- src/main/scala/kuram/monoid/laws.scala
+++ src/main/scala/kuram/monoid/laws.scala
@@ -24,24 +24,24 @@ package monoid
 
 package object laws {
   trait MonoidLaws[T] {
-    implicit def M: Monoid[T]
+    implicit def F: Monoid[T]
 
     /** must obey: `id + x == x + id` */
-    def identityLaw(x: T): Boolean =
-      (M.combine(M.empty, x) == x) == (M.combine(x, M.empty) == x)
+    def identity(x: T): Boolean =
+      (F.combine(F.empty, x) == x) == (F.combine(x, F.empty) == x)
 
     /** must obey: `a + (b + c) = (a + b) + c` */
     def associativity(a: T, b: T, c: T): Boolean =
-      M.combine(a, M.combine(b, c)) == M.combine(M.combine(a, b), c)
+      F.combine(a, F.combine(b, c)) == F.combine(F.combine(a, b), c)
 
     /** must obey: `f(a) + f(b) == f(a + b)` */
     def homomorphism[U](a: T, b: T)(f: T => U)(using U: Monoid[U]): Boolean =
-      U.combine(f(a), f(b)) == f(M.combine(a, b))
+      U.combine(f(a), f(b)) == f(F.combine(a, b))
   }
 
   object MonoidLaws {
     def apply[T](using monoid: Monoid[T]): MonoidLaws[T] = new {
-      implicit def M: Monoid[T] = monoid
+      implicit def F: Monoid[T] = monoid
     }
   }
 }
