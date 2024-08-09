23a555adce0d82c207a7d696358dcaffb476547d
csgn <dev.csgn@gmail.com>
2024-08-09 23:16:51 +0300

    test: add tests for Semigroup

--- tests/src/test/scala/kuram/SemigroupSuite.scala
+++ tests/src/test/scala/kuram/SemigroupSuite.scala
@@ -22,90 +22,70 @@
 package kuram
 
 import semigroup.Semigroup
-import semigroup.syntax.*
 import semigroup.instances.all.given
+import semigroup.laws.{SemigroupLaws => laws}
 
 class SemigroupSuite extends munit.FunSuite {
-  test("combine int values") {
-    val (x, y) = (1, 2)
+  test("Should satisfy associativity") {
+    List(
+      (1, 2, 3),
+      (100, 200, 300),
+      (1, 10, 100)
+    ).foreach {
+      case (a, b, c) => {
+        val obtained = laws(using Semigroup[Int]).associativity(a, b, c)
+        assertEquals(obtained, true)
+      }
+    }
 
-    val expected = x + y
-    val obtained1 = Semigroup[Int].combine(x, y)
-    val obtained2 = x |+| y
+    List(
+      ("h", "", "world"),
+      ("hello", "beautiful", "world"),
+      ("", "", "w")
+    ).foreach {
+      case (a, b, c) => {
+        val obtained = laws(using Semigroup[String]).associativity(a, b, c)
+        assertEquals(obtained, true)
+      }
+    }
 
-    assertEquals(obtained1, expected)
-    assertEquals(obtained2, expected)
+    List(
+      (List(1, 2), List(), List(3, 4)),
+      (List(), List(), List(3, 4, 5)),
+      (List(1), List(2), List(3, 4, 5)),
+      (List(), List(), List())
+    ).foreach {
+      case (a, b, c) => {
+        val obtained = laws(using Semigroup[List[Int]]).associativity(a, b, c)
+        assertEquals(obtained, true)
+      }
+    }
+
+    List(
+      (List("hello"), List(), List("world")),
+      (List("h"), List("w"), List("l")),
+      (List("hello"), List("beautiful"), List("world")),
+      (List(), List(), List())
+    ).foreach {
+      case (a, b, c) => {
+        val obtained = laws(using Semigroup[List[String]]).associativity(a, b, c)
+        assertEquals(obtained, true)
+      }
+    }
+
   }
 
-  test("combine string values") {
-    val (s1, s2) = ("foo", "bar")
-
-    val expected = s1 + s2
-    val obtained1 = Semigroup[String].combine(s1, s2)
-    val obtained2 = s1 |+| s2
-
-    assertEquals(obtained1, expected)
-    assertEquals(obtained2, expected)
-  }
-
-  test("combine list of integer values") {
-    val (l1, l2) = (List(1, 2, 3), List(4, 5))
-
-    val expected = l1 ++ l2
-    val obtained1 = Semigroup[List[Int]].combine(l1, l2)
-    val obtained2 = l1 |+| l2
-
-    assertEquals(obtained1, expected)
-    assertEquals(obtained2, expected)
-  }
-
-  test("combine list of string values") {
-    val (l1, l2) = (List("a", "b", "c"), List("d", "e"))
-
-    val expected = l1 ++ l2
-    val obtained1 = Semigroup[List[String]].combine(l1, l2)
-    val obtained2 = l1 |+| l2
-
-    assertEquals(obtained1, expected)
-    assertEquals(obtained2, expected)
-  }
-
-  test("combine list of integer lists lists") {
-    val (l1, l2) = (List(List(1), List(2, 3), List(4)), List(List(5, 6), List(7)))
-
-    val expected = l1 ++ l2
-    val obtained1 = Semigroup[List[List[Int]]].combine(l1, l2)
-    val obtained2 = l1 |+| l2
-
-    assertEquals(obtained1, expected)
-    assertEquals(obtained2, expected)
-  }
-
-  test("associativity") {
-    val (a, b, c) = (1, 2, 3)
-
-    val expected1 = a + (b + c)
-    val expected2 = (a + b) + c
-
-    val obtained1 = a |+| (b |+| c)
-    val obtained2 = (a |+| b) |+| c
-
-    assertEquals(obtained1, expected1)
-    assertEquals(obtained2, expected2)
-    assertEquals(obtained1, obtained2)
-  }
-
-  test("homomorphism") {
-    val (s1, s2) = ("hello", "world")
-
-    val expected1 = s1.length + s2.length
-    val expected2 = (s1 + s2).length
-
-    val obtained1 = s1.length |+| s2.length
-    val obtained2 = (s1 |+| s2).length
-
-    assertEquals(obtained1, expected1)
-    assertEquals(obtained2, expected2)
-    assertEquals(obtained1, obtained2)
+  test("Should satisfy homomorphism") {
+    List(
+      ("", ""),
+      ("hello", ""),
+      ("", "world"),
+      ("hello", "world")
+    ).foreach {
+      case (s1, s2) => {
+        val obtained = laws(using Semigroup[String]).homomorphism(s1, s2)(_.length)
+        assertEquals(obtained, true)
+      }
+    }
   }
 }
