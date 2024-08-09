183a2d48ab5b89524f107b84e4196871a604d742
csgn <dev.csgn@gmail.com>
2024-08-09 23:17:34 +0300

    feat: MonoidLaws must extends SemigroupLaws

--- src/main/scala/kuram/monoid/laws.scala
+++ src/main/scala/kuram/monoid/laws.scala
@@ -22,21 +22,15 @@
 package kuram
 package monoid
 
+import semigroup.laws.SemigroupLaws
+
 package object laws {
-  trait MonoidLaws[T] {
+  trait MonoidLaws[T] extends SemigroupLaws[T] {
     implicit def F: Monoid[T]
 
     /** must obey: `id + x == x + id` */
     def identity(x: T): Boolean =
       (F.combine(F.empty, x) == x) == (F.combine(x, F.empty) == x)
-
-    /** must obey: `a + (b + c) = (a + b) + c` */
-    def associativity(a: T, b: T, c: T): Boolean =
-      F.combine(a, F.combine(b, c)) == F.combine(F.combine(a, b), c)
-
-    /** must obey: `f(a) + f(b) == f(a + b)` */
-    def homomorphism[U](a: T, b: T)(f: T => U)(using U: Monoid[U]): Boolean =
-      U.combine(f(a), f(b)) == f(F.combine(a, b))
   }
 
   object MonoidLaws {
