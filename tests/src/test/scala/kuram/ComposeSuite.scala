fc7e0a19faf34d54df11ab940ee88e75d34f27f7
csgn <dev.csgn@gmail.com>
2024-08-10 21:17:24 +0300

    test: tests for Compose

--- tests/src/test/scala/kuram/ComposeSuite.scala
+++ tests/src/test/scala/kuram/ComposeSuite.scala
@@ -21,53 +21,18 @@
 
 package kuram
 
+import compose.instances.all.given
 import compose.syntax.*
-import category.instances.function1.given
 
 class ComposeSuite extends munit.FunSuite {
-  test("apply last function") {
-    val addOne: Int => Int = _ + 1
-    val multiplyByTwo: Int => Int = _ * 2
+  test("Should satisfy associativity") {
+    val f: Function1[Int, Int] = _ + 1
+    val g: Function1[Int, Int] = _ + 2
+    val h: Function1[Int, Int] = _ + 3
 
-    val randomValue = System.currentTimeMillis().toInt
-    val expected = addOne.compose(multiplyByTwo)(randomValue)
-    val obtained = (addOne <<< multiplyByTwo)(randomValue)
+    val as1 = f >>> (g >>> h)
+    val as2 = (f >>> g) >>> h
 
-    assertEquals(obtained, expected)
-  }
-
-  test("apply first function") {
-    val addOne: Int => Int = _ + 1
-    val multiplyByTwo: Int => Int = _ * 2
-
-    val randomValue = System.currentTimeMillis().toInt
-    val expected = addOne.andThen(multiplyByTwo)(randomValue)
-    val obtained = (addOne >>> multiplyByTwo)(randomValue)
-
-    assertEquals(obtained, expected)
-  }
-
-  test("associativity") {
-    val f: Int => Int = _ + 1
-    val g: Int => Int = _ * 2
-    val h: Int => Int = _ - 1
-
-    val randomValue = System.currentTimeMillis().toInt
-    val expectedLast1 = f.compose(g.compose(h))(randomValue)
-    val obtainedLast1 = (f <<< (g <<< h))(randomValue)
-
-    val expectedLast2 = f.compose(g).compose(h)(randomValue)
-    val obtainedLast2 = ((f <<< g) <<< h)(randomValue)
-
-    val expectedFirst1 = f.andThen(g.andThen(h))(randomValue)
-    val obtainedFirst1 = (f >>> (g >>> h))(randomValue)
-
-    val expectedFirst2 = f.andThen(g).andThen(h)(randomValue)
-    val obtainedFirst2 = ((f >>> g) >>> h)(randomValue)
-
-    assertEquals(obtainedLast1, expectedLast1)
-    assertEquals(obtainedLast2, expectedLast2)
-    assertEquals(obtainedFirst1, expectedFirst1)
-    assertEquals(obtainedFirst2, expectedFirst2)
+    assertEquals(as1(1), as2(1))
   }
 }
