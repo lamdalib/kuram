cf43db1a04c8149727da0ecf7e3dea62d128e47b
csgn <dev.csgn@gmail.com>
2024-09-14 12:25:55 +0300

    test: add tests for FlatMap

--- tests/src/test/scala/kuram/tests/FlatMapSuite.scala
+++ tests/src/test/scala/kuram/tests/FlatMapSuite.scala
@@ -1,3 +1,38 @@
+/*
+ * Copyright (c) 2024 kattulib
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package kuram.tests
 
-class FlatMapSuite extends munit.FunSuite {}
+import kuram.data.Id
+import kuram.instances.id.given
+import kuram.syntax.eq.*
+import kuram.laws.FlatMapLaws
+
+class FlatMapSuite extends munit.FunSuite {
+  private val laws = FlatMapLaws[Id]
+
+  test("associativity") {
+    val f: String => Id[Int] = Id(_.length)
+    val g: Int => Int = Id(_ * 2)
+    val isEq = laws.associativity(Id("test"))(f, g)
+    assert(isEq.a === isEq.b)
+  }
+}
