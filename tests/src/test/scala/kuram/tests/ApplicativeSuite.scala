a5e9aefb7d9657907fc46f2f3234b95fd2b8dc83
csgn <dev.csgn@gmail.com>
2024-09-14 11:54:19 +0300

    test: add tests for Applicative

--- tests/src/test/scala/kuram/tests/ApplicativeSuite.scala
+++ tests/src/test/scala/kuram/tests/ApplicativeSuite.scala
@@ -21,4 +21,38 @@
 
 package kuram.tests
 
-class ApplicativeSuite extends munit.FunSuite {}
+import kuram.laws.ApplicativeLaws
+import kuram.data.Id
+import kuram.instances.id.given
+import kuram.syntax.eq.*
+
+class ApplicativeSuite extends munit.FunSuite {
+  private val laws = ApplicativeLaws[Id]
+
+  test("identity") {
+    val isEq = laws.identity(Id(1))
+    assert(isEq.a === isEq.b)
+  }
+
+  test("leftIdentity") {
+    val isEq = laws.leftIdentity(Id(1))
+    assert(isEq.a === isEq.b)
+  }
+
+  test("rightIdentity") {
+    val isEq = laws.rightIdentity(Id(1))
+    assert(isEq.a === isEq.b)
+  }
+
+  test("homomorphism") {
+    val f: String => Int = _.length
+    val isEq = laws.homomorphism("test")(f)
+    assert(isEq.a === isEq.b)
+  }
+
+  test("interchange") {
+    val f: String => Int = _.length
+    val isEq = laws.interchange("test")(Id(f))
+    assert(isEq.a === isEq.b)
+  }
+}
