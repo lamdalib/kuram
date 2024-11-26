8f711a412a930a503947827c61684f48487d4562
csgn <dev.csgn@gmail.com>
2024-11-26 21:03:52 +0300

    refactor: add final infix prefix to operators

--- core/src/main/scala/kuram/syntax/eq.scala
+++ core/src/main/scala/kuram/syntax/eq.scala
@@ -24,9 +24,9 @@ package syntax
 
 private[syntax] trait EqSyntax {
   extension [T](a: T)(using Eq[T]) {
-    def ===(b: T): Boolean = Eq[T].eqv(a, b)
-    def =!=(b: T): Boolean = Eq[T].neqv(a, b)
-    def eqv(b: T): Boolean = Eq[T].eqv(a, b)
-    def neqv(b: T): Boolean = Eq[T].neqv(a, b)
+    final infix def ===(b: T): Boolean = Eq[T].eqv(a, b)
+    final infix def =!=(b: T): Boolean = Eq[T].neqv(a, b)
+    final infix def eqv(b: T): Boolean = Eq[T].eqv(a, b)
+    final infix def neqv(b: T): Boolean = Eq[T].neqv(a, b)
   }
 }
