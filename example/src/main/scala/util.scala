155a4ecfca5b351c9b2b8a520d2879ba7efaed38
csgn <dev.csgn@gmail.com>
2024-11-26 21:08:47 +0300

    chore: add newline in order to see properly

--- example/src/main/scala/util.scala
+++ example/src/main/scala/util.scala
@@ -24,6 +24,6 @@ package example
 def assertAndPrint[A, B](inputs: List[(A, B)], solve: A => B): Unit =
   inputs.foreach { (input, expected) =>
     val obtained = solve(input)
-    println(s"Obtained: $obtained, Expected: $expected")
+    println(s"Obtained: $obtained\nExpected: $expected")
     assert(obtained == expected)
   }
