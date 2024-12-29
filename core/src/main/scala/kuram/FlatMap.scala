dd974d668e566eefe6f192e65b02904fffb07950
csgn <dev.csgn@gmail.com>
2024-12-29 21:37:24 +0300

    feat: add new combinator

--- core/src/main/scala/kuram/FlatMap.scala
+++ core/src/main/scala/kuram/FlatMap.scala
@@ -24,6 +24,8 @@ package kuram
 trait FlatMap[F[_]] extends Apply[F] {
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
+  def flatten[A](ffa: F[F[A]]): F[A] = flatMap(ffa)(identity)
+
   override def ap[A, B](ff: F[A => B])(fa: F[A]): F[B] =
     flatMap(fa)(a => {
       map(ff)(f => f(a))
